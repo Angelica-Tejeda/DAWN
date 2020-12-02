ab2eddb14d5790b6374c071e762f18f4f26e7f12
daniel sanchez <dsjsanchez@hotmail.com>
2020-12-02 16:08:32 -0500

    menu

---
+++ menu.js
@@ -0,0 +1,89 @@
+const buscador = document.getElementById("myInput");
+buscador.addEventListener('input',function(event){
+  let frase = event.target.value.toUpperCase()
+  let burbujas = document.getElementsByClassName("burbuja")
+  if(frase != ""){
+    for(let item of burbujas){
+        let autor = item.getElementsByTagName("h2")[0].textContent.toUpperCase()
+        if(autor.includes(frase.toUpperCase())){
+            item.style.display = "grid"
+        }
+        else{
+            item.style.display = "none"
+        }
+
+      }
+  }else{
+    let contador = 0
+    for(let item of burbujas){
+      if(contador < 10){
+        item.style.display = "grid"
+      }
+      else{
+        item.style.display = "none"
+      }
+      contador = contador + 1
+    }
+  }
+
+
+});
+
+
+
+
+let cargarDatos = () => {
+    let divInicial = document.getElementById("menub")
+    
+    fetch("menu.json")
+    .then((resultado) =>{
+        return resultado.json();
+    })
+    .then((json) =>{
+        
+       var x = 1
+        for(let elemento of json){
+           let div = document.createElement("div")
+           div.setAttribute("class","menud")
+           div.setAttribute("id","menudiv")     
+           
+           let div1 = document.createElement("div")
+           div1.setAttribute("class","textodiv")
+           div1.setAttribute("id","textiodiv")     
+
+           let nombre = document.createElement("h2")
+           nombre.textContent = elemento.nombre;
+          nombre.setAttribute("class","nombreMenu")
+
+         
+          let textot = document.createElement("h3")
+          nombre.textContent = elemento.nombre;
+          nombre.setAttribute("class","textom")
+          textot.textContent = elemento.texto 
+
+          let imgd = document.createElement("div")
+           imgd.setAttribute("class","imagenesmenu")
+           let img = document.createElement("img");
+           img.src = elemento.src;
+           img.setAttribute("class","img")
+           imgd.appendChild(img)
+          
+          
+          
+          div1.appendChild(nombre);
+          div1.appendChild(textot);
+          div.append(div1)
+          div.appendChild(imgd);
+         
+         
+          divInicial.appendChild(div)
+           
+        }
+
+    })
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    cargarDatos();
+})
